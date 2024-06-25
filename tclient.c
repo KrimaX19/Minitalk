c0f30aa073847f3a5869c52579d8ef3f4913afeb
Rui Miguel Soares Martins <rusoares@c2r6s8.42porto.com>
2024-06-25 22:11:12 +0100

    2nd test

--- tclient.c
+++ tclient.c
@@ -6,7 +6,7 @@
 /*   By: rusoares <rusoares@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/19 18:24:46 by rusoares          #+#    #+#             */
-/*   Updated: 2024/06/20 18:07:54 by rusoares         ###   ########.fr       */
+/*   Updated: 2024/06/25 22:07:32 by rusoares         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,24 @@
 #include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
+
+void	message(unsigned int pid_server, unsigned char msg)
+{
+	int	nbits;
+
+	nbits = 8;
+	while (msg)
+	{
+		while (--nbits >= 0)
+		{
+			if (msg &  (1 << 1))
+				kill(pid_server, SIGUSR1);
+			else
+				kill(pid_server, SIGUSR2);
+			usleep (300);
+		}
+	}
+}
 
 void	signal_handler(int sig)
 {
@@ -29,17 +47,31 @@ void	signal_handler(int sig)
 int	main(int argc, char* argv[])
 {
 	int	pidserver;
-	int	pid;
-	char ok[] = {0xE2, 0x9C, 0x93};
+	// int	pid;
+	int	i = 0;
+	// int	msg;
+	// char ok[] = {0xE2, 0x9C, 0x93};
+	const char *str;
 	
-	pid = getpid();
+	// pid = getpid();
 	pidserver = atoi(argv[1]);
-	printf("PID: %d\n", pid);
-	sleep(10);
-	signal(SIGUSR1, signal_handler);
-	write(1, ok, 3);
-	write(1, "  Message Sent\n", 15);
-	kill (pidserver, SIGUSR1);
-	sleep(10);
+	str = argv[2];
+	// msg = 0;
+	// printf("PID: %d\n", pid);
+	// sleep(10);
+	while (str[i])
+	{
+		message(pidserver, str[i++]);
+	}
+	message(pidserver, '\0');
+	// if (msg == 1)
+	// {
+	// 	write(1, ok, 3);
+	// 	write(1, "  Message Sent\n", 15);
+	// }
+	// else
+	// 	write (1, "Failed to send\n", 15);
+	// signal(SIGUSR1, signal_handler);
+	// sleep(10);
 	return (0);
 }
--- tserver.c
+++ tserver.c
@@ -6,7 +6,7 @@
 /*   By: rusoares <rusoares@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/19 18:27:14 by rusoares          #+#    #+#             */
-/*   Updated: 2024/06/20 18:51:43 by rusoares         ###   ########.fr       */
+/*   Updated: 2024/06/25 22:09:40 by rusoares         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,16 +15,35 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define END_TRANSMISSION '\0'
+
 int pid_cl = 0;
 
-void	signal_handler(int sig)
+void	signal_handler(int signal)
 {
-	if (sig == SIGUSR1)
+	static unsigned char	current_char;
+	static int				bit_index;
+
+	current_char |= (signal == SIGUSR1);
+	bit_index++;
+	if (bit_index == 8)
 	{
-		kill(pid_cl, SIGUSR1);
-		write(1 , "SIGNAL RECEIVED\n", 16);
+		if (current_char == END_TRANSMISSION)
+			printf("\n");
+		else
+			printf("%c", current_char);
+		bit_index = 0;
+		current_char = 0;
 	}
+	else
+		current_char <<= 1;
 }
+	// if (sig == SIGUSR1)
+	// {
+	// 	kill(pid_cl, SIGUSR1);
+	// 	write(1 , "Signal Received\n", 16);
+	// }
+
 int	main(int ac, char **av)
 {
 	int	pid;
@@ -32,17 +51,18 @@ int	main(int ac, char **av)
 	int* client_pid;
 
 	pid = getpid();
-	printf("PID: %d\n", pid_cl);
+	// printf("PID: %d\n", pid_cl);
 	printf("PID: %d\n", pid);
-	printf("PID: %d\n", pid_cl);
+	// printf("PID: %d\n", pid_cl);
 	signal(SIGUSR1, signal_handler);
 	signal(SIGUSR2, signal_handler);
-	printf("Enter Client PID:");
-	scanf("%d", client_pid);
+	// printf("Enter Client PID:");
+	// scanf("%d", client_pid);
 	pid_cl = *client_pid;
 	printf("PID: %d\n", pid_cl);
 	while (1)
 	{
+		pause();
 	}
 	return(0);
 }
