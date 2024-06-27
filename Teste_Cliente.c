6efbdc9548816d9490741e94757cf7a91774b759
Rui Miguel Soares Martins <rusoares@c2r6s8.42porto.com>
2024-06-27 22:44:15 +0100

    3rd teste

--- Teste_Cliente.c
+++ Teste_Cliente.c
@@ -6,24 +6,37 @@
 /*   By: rusoares <rusoares@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/27 19:06:08 by rusoares          #+#    #+#             */
-/*   Updated: 2024/06/27 22:38:39 by rusoares         ###   ########.fr       */
+/*   Updated: 2024/06/27 22:44:11 by rusoares         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 #include <signal.h>
-void	send_message(int pid, const char *message)
+void	send_pid(int pid, const char *message)
 {
 	int	client;
 	int	i;
+	int j;
 	char byte;
+	int bit;
 
 	client = getpid();
 	i = sizeof(int) - 1;
-	client >> (i * 8) & 
+	j = 8;
 	while (i >= 0)
 	{
-		
+		byte = (client >> (i * 8)) & 255;
+		while (j > 0)
+		{
+			bit = (byte >> j) & 1;
+			if (bit == 0)
+				kill(pid, SIGUSR1);
+			else
+				kill(pid, SIGUSR2);
+			uslepp(50);
+			j--;
+		}
+		i--;
 	}
 }
 
@@ -45,7 +58,8 @@ int	main(int ac, char **av)
 	message = av[2];
 	sigemptyset(&sa.sa_mask);
 	sigaction(SIGUSR1, &sa, NULL);
-	send_message(server_pid, message);
+	sendpid(server_pid, message);
+	send_message()
 	if (ac != 3)
 	{
 		write(1, "Use in this format: <server-pid> <message>\n", 44);
